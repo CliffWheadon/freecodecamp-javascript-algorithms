ad12415dd2c6269f2a48298659422c67695b4c65
Cliff Wheadon <cliff.wheadon@nielsen.com>
2020-02-09 20:38:37 -0400

    Fixing Test file

--- test/convertHTML.test.js
+++ test/convertHTML.test.js
@@ -6,13 +6,4 @@ test('convertHTML("Dolce & Gabbana") should return Dolce &amp; Gabbana', () => {
 
 test('convertHTML("Hamburgers < Pizza < Tacos") should return Hamburgers &lt; Pizza &lt; Tacos', () => {
     expect(convertHTML("Hamburgers < Pizza < Tacos")).toBe('Hamburgers &lt; Pizza &lt; Tacos');
-})
-/*
-convertHTML("Dolce & Gabbana") should return Dolce &amp; Gabbana.
-convertHTML("Hamburgers < Pizza < Tacos") should return Hamburgers &lt; Pizza &lt; Tacos.
-convertHTML("Sixty > twelve") should return Sixty &gt; twelve.
-convertHTML('Stuff in "quotation marks"') should return Stuff in &quot;quotation marks&quot;.
-convertHTML("Schindler's List") should return Schindler&apos;s List.
-convertHTML("<>") should return &lt;&gt;.
-convertHTML("abc") should return abc.
- */
+});
