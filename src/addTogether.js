ca498a03647011d060d74ce6c8c40e15416b7c94
Cliff Wheadon <cliff.wheadon@nielsen.com>
2020-03-26 22:02:18 -0300

    Flipping y and x to make things read better.

--- src/addTogether.js
+++ src/addTogether.js
@@ -2,10 +2,10 @@ function addTogether() {
     if (arguments.length === 2 && Number.isInteger(arguments[0]) && Number.isInteger(arguments[1])) {
         return arguments[0] + arguments[1];
     } else if (arguments.length === 1 && Number.isInteger(arguments[0])) {
-        const y = arguments[0];
-        return function (x) {
-            if (Number.isInteger(x)) {
-                return y + x;
+        const x = arguments[0];
+        return function (y) {
+            if (Number.isInteger(y)) {
+                return x + y;
             }
             return undefined;
         }
